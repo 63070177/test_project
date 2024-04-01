d8d3e60172dc767664cd53553d2aa46689ae3fe7
63070177 <73645471+63070177@users.noreply.github.com>
2024-04-01 11:16:51 +0700

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extened: true}))
+
+const doctorRouter = require('./routes/doctor')
+const patientsRouter = require('./routes/patients')
+const formulaRouter = require('./routes/formula')
+const appointmentRouter = require('./routes/appointment')
+const bloodResultRouter = require('./routes/bloodresult')
+const feedbackRouter = require('./routes/feedback')
+const userRouter = require('./routes/user')
+//const ocrRouter = require('./routes/ocr')
+
+app.use(doctorRouter.router)
+app.use(patientsRouter.router)
+app.use(formulaRouter.router)
+app.use(appointmentRouter.router)
+app.use(bloodResultRouter.router)
+app.use(feedbackRouter.router)
+app.use(userRouter.router)
+app.use(express.static('uploads'))
+//app.use(ocrRouter.router)
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000')
+})
